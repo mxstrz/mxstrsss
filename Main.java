ae43037080b2564b6beac40fce8395d0b6597a59
mxstrz <dubianmycola@gmail.com>
2024-11-27 21:04:35 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/mxstrz/mxstrsss.git
+//git push -u origin main
